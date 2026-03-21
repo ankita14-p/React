b305536bf7b134bd37caf454154e0ff7b8a340f4
Ankita Nag <ankeetaa3@gmail.com>
2026-03-21 12:59:49 +0530

    component based learning

---
+++ 02_components/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const card = () => {
+ 
+ return (<div>
+      <div className="card">
+        <h1>Ankita Nag</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, expedita.</p>
+      </div>
+    </div>)
+
+
+}
+
+export default card
